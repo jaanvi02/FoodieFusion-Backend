86eb143f0f1b9298bc6b0fc79f9061dbe295bbb9
Jaanvi Chaudhary <89679703+jaanvi02@users.noreply.github.com>
2023-09-30 18:42:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the PORT environment variable if available
+
 const bodyParser = require('body-parser');
-const mongoDB=require("./db")
-const path=require('path')
-const baseurl="https://mernapp1-961s.onrender.com/";
-mongoDB();
+const mongoDB = require('./db');
+
+mongoDB(); // Initialize your MongoDB connection
+
+// CORS Middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://incredible-meerkat-a6015d.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -13,20 +15,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Parse JSON requests
+app.use(express.json());
 
-app.use(express.json())
-app.use('/api',require("./Routes/Createuser"))
-app.use('/api',require("./Routes/Displaydata"))
-app.use('/api',require("./Routes/OrderData"))
+// Routes
+app.use('/api', require('./Routes/Createuser'));
+app.use('/api', require('./Routes/Displaydata'));
+app.use('/api', require('./Routes/OrderData'));
+
+// Body-parser Middleware (you don't need both bodyParser.json() and express.json())
 app.use(bodyParser.json());
+
+// Example POST route
 app.post('/', (req, res) => {
- res.send("hello")
-  
+  res.send('Hello');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
