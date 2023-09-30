238fea73522b205fdc964a7e969bf35e34346d96
Jaanvi Chaudhary <89679703+jaanvi02@users.noreply.github.com>
2023-09-30 18:33:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path=require('path')
 const baseurl="https://mernapp1-961s.onrender.com/";
 mongoDB();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${baseurl}`);
+  res.setHeader('Access-Control-Allow-Origin', 'https://incredible-meerkat-a6015d.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
