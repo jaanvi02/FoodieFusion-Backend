9c04872314c483f871f851bb142ec3faa2d87986
Jaanvi Chaudhary <89679703+jaanvi02@users.noreply.github.com>
2023-09-30 18:26:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 const bodyParser = require('body-parser');
 const mongoDB=require("./db")
+const path=require('path')
+const baseurl="https://mernapp1-961s.onrender.com/";
 mongoDB();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Origin', `${baseurl}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
@@ -24,6 +26,7 @@ app.post('/', (req, res) => {
  res.send("hello")
   
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
